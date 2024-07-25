fixed a loooooooooooooot of things , logging , loss functions verified, logged teacher against student masks,made sure no bottlenecks in dataloading , and one hot masking is correct and speedy ,i 'm proud hhhhhh

workstation/src/Unet.py
@@ -6,9 +6,9 @@ torch.manual_seed(50)
 class conv_block(nn.Module):
     def __init__(self, in_c, out_c,negative_slope):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=7, padding=3)
-        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=5, padding=2)
-        self.conv3 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1,bias=False)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1,bias=False)
+        self.conv3 = nn.Conv2d(out_c, out_c, kernel_size=7, padding=3,bias=False)
         self.bn = nn.BatchNorm2d(out_c)
         self.relu = nn.LeakyReLU(negative_slope=negative_slope)
 
@@ -50,7 +50,7 @@ class segUnet(nn.Module):
         self.depth = depth
         self.negative_slope=negative_slope
         self.internal_masks=[]
-        
+        self.skips=[]
 
         """ Encoders """
         self.encoders = nn.ModuleList([encoder_block(in_channels, start_filts,self.negative_slope)])
@@ -65,30 +65,37 @@ class segUnet(nn.Module):
         """ Classifier """
         self.outputs = nn.Conv2d(start_filts, num_classes, kernel_size=1)
 
-    def forward(self, inputs,visualize_mask=False):
-        skips = []
-        self.internal_masks=[]
+    def forward(self, inputs):
         x = inputs
-        self.internal_masks.append(x[-3,-1])
-
+        self.skips=[]
         for encoder in self.encoders:
             x, p = encoder(x)
-            if visualize_mask:
-                self.internal_masks.extend([x[-3,-1],p[-3,-1]])
-            skips.append(x)
+            self.skips.append(x)
             x = p
 
         x = self.bottleneck(x)
-        if visualize_mask:
-            self.internal_masks.append(x[-3,-1])
 
         for i, decoder in enumerate(self.decoders):
-            x = decoder(x, skips[-(i+1)])
-            if visualize_mask:
-                self.internal_masks.append(x[-3,-1])
+            x = decoder(x, self.skips[-(i+1)])
         outputs = self.outputs(x)
-        if visualize_mask:
-            self.internal_masks.append(np.argmax(outputs[-3].detach().permute(1,2,0).numpy(),axis=-1))
-            return self.internal_masks
         return outputs
     
+    def exp_forward(self,inputs):
+        self.skips = []
+        self.internal_masks=[]
+        x = inputs
+        self.internal_masks.append(x[-1])
+        for encoder in self.encoders:
+            x, p = encoder(x)
+            self.internal_masks.append(x[-1,-1])
+            self.skips.append(x)
+            x = p
+        self.internal_masks.append(x[-1,-1])
+        x = self.bottleneck(x)
+
+        for i, decoder in enumerate(self.decoders):
+            self.internal_masks.append(x[-1,-1])
+            x = decoder(x, self.skips[-(i+1)])
+        
+        return self.internal_masks
+        

workstation/src/dataset.py
@@ -3,8 +3,8 @@ from torch.utils.data import Dataset
 import numpy as np
 from pathlib import Path
 from PIL import Image
-from .utils import cityscapesMaskProcessor,bdd10kMaskProcessor,load_yaml
-
+from utils import  load_yaml,UnetMaskProcessor
+from tqdm.notebook import trange
 torch.manual_seed(50)
 
 class SegDataset(Dataset):
@@ -14,29 +14,35 @@ class SegDataset(Dataset):
             config = load_yaml()
         self.cfg = config
         self.Unet_cfg = load_yaml(self.cfg['paths']['cfg']['Unet'])
-
+ 
         self.img_dir = Path(self.cfg['dataset'][f'train_imgs_dir'])
         self.mask_dir = Path(self.cfg['dataset'][f'train_masks_dir'])
-
+ 
         self.img_paths = sorted(list(self.img_dir.glob('*.'+self.cfg['dataset']['img_type'])))
         self.mask_paths = sorted(list(self.mask_dir.glob('*.'+self.cfg['dataset']['mask_type'])))
-
+ 
         assert len(self.img_paths) == len(self.mask_paths), f"Number of images and masks do not match\  {len(self.img_paths)} does not match {len(self.mask_paths)} "
-
+        self.images=[]
+        self.masks= []
+        self.maskpreprocessor=UnetMaskProcessor()
+        self.load_data_into_memory()
+ 
     def __len__(self):
         return len(self.img_paths)
-
+    def load_data_into_memory(self):
+        for i in trange(len(self.img_paths),desc="Loading data into memory"):
+            img_path=self.img_paths[i]
+            mask_path=self.mask_paths[i]
+            image=Image.open(img_path).resize(
+                (self.Unet_cfg['resize'][1],self.Unet_cfg['resize'][0]),resample=Image.BILINEAR)
+            image=torch.from_numpy(np.array(image)/255).float().permute(2,0,1)
+            mask=Image.open(mask_path).resize(
+                (self.Unet_cfg['resize'][1],self.Unet_cfg['resize'][0]),resample=Image.NEAREST)
+            mask=self.maskpreprocessor.one_hotting_mask(np.array(mask))
+            mask=torch.from_numpy(mask)
+            self.images.append(image)
+            self.masks.append(mask)
+ 
     def __getitem__(self, index):
-        img_path = self.img_paths[index]
-        mask_path = self.mask_paths[index]
-
-        maskprocessor=bdd10kMaskProcessor()
-
-        image = Image.open(img_path).resize((self.Unet_cfg['resize'][1], self.Unet_cfg['resize'][0]), resample=Image.BILINEAR)
-        image = torch.from_numpy(np.array(image) / 255).float().permute(2, 0, 1)  # Normalize and permute to [C, H, W]
-
-        mask = Image.open(mask_path).resize((self.Unet_cfg['resize'][1], self.Unet_cfg['resize'][0]), resample=Image.NEAREST)
-        mask=maskprocessor.one_hotting_mask(np.array(mask))
-        mask = torch.from_numpy(mask)
-
-        return image, mask
+        return self.images[index],self.masks[index]
+ 

workstation/src/loss.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from .utils import load_yaml
+from utils import load_yaml
 
 torch.manual_seed(50)
 
@@ -25,7 +25,6 @@ class WeightedCELoss(nn.Module):
         true_mask = true_mask.permute(0, 2, 3, 1).reshape(-1, true_mask.size(1))
 
         loss = nn.functional.cross_entropy(out_mask, true_mask.argmax(dim=1),weight=self.weights, reduction='mean')
-
         return loss
 
 class TverskyCEDiceWeightedLoss(nn.Module):
@@ -37,7 +36,7 @@ class TverskyCEDiceWeightedLoss(nn.Module):
 class WeightedDistillationLoss(nn.Module):
     """Distillation loss."""
     def __init__(self,temperature,alpha=0.5):
-        super(WeightedDistillationLoss, self).__init__()
+        super().__init__()
         self.ce_loss = WeightedCELoss()
         self.temperature = temperature
         self.alpha = alpha
@@ -49,3 +48,7 @@ class WeightedDistillationLoss(nn.Module):
         loss = self.alpha * soft_loss + (1 - self.alpha) * hard_loss
 
         return loss
+    
+    def to(self,device):
+        self.ce_loss=self.ce_loss.to(device)
+        return super(WeightedDistillationLoss,self).to(device) 

workstation/src/teacher_engine.py
@@ -1,10 +1,10 @@
-from tqdm import trange, tqdm
+from tqdm.notebook import trange,tqdm
 import numpy as np
 import torch
 import wandb
 from segmentation_models_pytorch.metrics import iou_score, f1_score, recall, get_stats
 
-from ....utils import  load_yaml,cityscapesMaskProcessor,bdd10kMaskProcessor
+from utils import  load_yaml,UnetMaskProcessor
 
 torch.manual_seed(50)
 
@@ -46,7 +46,7 @@ def return_loggable_imgs(images, masks):
         masks = masks.permute(0, 2, 3, 1).cpu().numpy()
     
     single_channel_masks = np.argmax(masks, axis=-1)
-    class_labels = bdd10kMaskProcessor().class_labels
+    class_labels = UnetMaskProcessor().class_labels
     wandb_images = []
 
     for img, mask in zip(images, single_channel_masks):
@@ -74,13 +74,15 @@ def return_batch_metrics(criterion, outputs, masks):
 
 def train_step(model, dataloader, criterion, optimizer, accelerator, epoch):
 
-    if Unet_cfg['training']['last_batch']==-1:
+    if Unet_cfg['last_batch']==-1:
         last_batch = len(dataloader)
     else:
-        last_batch= Unet_cfg['training']['last_batch']
+        last_batch= Unet_cfg['last_batch']
     model.train()
     metrics = {'loss': 0.0, 'miou': 0.0, 'f1': 0.0, 'recall': 0.0, 'epoch':epoch}
-
+    tracked_images = None
+    tracked_masks = None
+    internal_masks=None
     for batch, (images, masks) in enumerate(tqdm(dataloader)):
         with accelerator.autocast():
             outputs = model(images)
@@ -91,9 +93,12 @@ def train_step(model, dataloader, criterion, optimizer, accelerator, epoch):
         optimizer.step()
         
         batch_metrics['loss'] = batch_metrics['loss'].item()
-        accelerator.log({"train_batches": batch_metrics, "batch": last_batch * epoch + batch+1})
+        accelerator.log({"train_batches": batch_metrics, "batch": last_batch * epoch + batch})
 
-    
+        if batch == Unet_cfg['img_sampling_index'] & epoch%Unet_cfg['log_masks_every']==0:
+            tracked_images = images
+            tracked_masks = outputs
+            internal_masks=model.exp_forward(images)
         if batch >= last_batch:
             break
 
@@ -102,13 +107,13 @@ def train_step(model, dataloader, criterion, optimizer, accelerator, epoch):
             metrics[key] += batch_metrics[key] / last_batch
 
 
-    return metrics
+    return metrics, tracked_images, tracked_masks, internal_masks
 
-def val_step(model, dataloader, criterion, accelerator, epoch, img_sampling_index):
-    if Unet_cfg['training']['last_batch']==-1:
+def val_step(model, dataloader, criterion, accelerator, epoch):
+    if Unet_cfg['last_batch']==-1:
         last_batch = len(dataloader)
     else:
-        last_batch= Unet_cfg['training']['last_batch']
+        last_batch= Unet_cfg['last_batch']
     model.eval()
     metrics = {'loss': 0.0, 'miou': 0.0, 'f1': 0.0, 'recall': 0.0,'epoch':epoch}
     tracked_images = None
@@ -122,13 +127,13 @@ def val_step(model, dataloader, criterion, accelerator, epoch, img_sampling_inde
                 batch_metrics = return_batch_metrics(criterion, outputs, masks)
         
         batch_metrics['loss'] = batch_metrics['loss'].item()
-        accelerator.log({"val_batches": batch_metrics, "batch": last_batch * epoch + batch+1})
+        accelerator.log({"val_batches": batch_metrics, "batch": last_batch * epoch + batch})
 
 
-        if batch == img_sampling_index & epoch%Unet_cfg['training']['log_masks_every']==0:
+        if batch == Unet_cfg['img_sampling_index'] & epoch%Unet_cfg['log_masks_every']==0:
             tracked_images = images
             tracked_masks = outputs
-            internal_masks=model(images,visualize_mask=True)
+            internal_masks=model.exp_forward(images)
         if batch >= last_batch:
             break
 
@@ -139,29 +144,35 @@ def val_step(model, dataloader, criterion, accelerator, epoch, img_sampling_inde
 
     return metrics, tracked_images, tracked_masks,internal_masks
 
-def engine(model, train_loader, val_loader, criterion, optimizer, scheduler, accelerator, epochs, img_sampling_index=10):
+def engine(model, train_loader, val_loader, criterion, optimizer, scheduler, accelerator, epochs):
 
     for epoch in trange(epochs):
         epoch_train_metrics = train_step(model, train_loader, criterion, optimizer, accelerator, epoch)
-        epoch_val_metrics = val_step(model, val_loader, criterion, accelerator, epoch, img_sampling_index)
-        scheduler.step(epoch_train_metrics['loss'])
-        if epoch%Unet_cfg['training']['log_masks_every']==0:
-            tracked_images = epoch_val_metrics[1]
-            tracked_masks = epoch_val_metrics[2]
-            wandb_img_and_masks = return_loggable_imgs(tracked_images, tracked_masks)
-            wandb_internal_representations=ready_internal_masks(epoch_val_metrics[3])
+        epoch_val_metrics = val_step(model, val_loader, criterion, accelerator, epoch)
+        scheduler.step(epoch_train_metrics[0]['loss'])
+        if epoch%Unet_cfg['log_masks_every']==0:
+            val_tracked_images = epoch_val_metrics[1]
+            val_tracked_masks = epoch_val_metrics[2]
+            val_wandb_img_and_masks = return_loggable_imgs(val_tracked_images, val_tracked_masks)
+            val_wandb_internal_representations=ready_internal_masks(epoch_val_metrics[3])
+            train_tracked_images = epoch_val_metrics[1]
+            train_tracked_masks = epoch_val_metrics[2]
+            train_wandb_img_and_masks = return_loggable_imgs(train_tracked_images, train_tracked_masks)
+            train_wandb_internal_representations=ready_internal_masks(epoch_train_metrics[3])
             learned_kernels=ready_kernels(model)
             epoch_metrics = {
-                'train_epochs': epoch_train_metrics,
+                'train_epochs': epoch_train_metrics[0],
                 'val_epochs': epoch_val_metrics[0],
                 'epoch': epoch,
-                'mask_per_epoch': wandb_img_and_masks,
-                'internals_per_epoch':wandb_internal_representations,
-                'filters_per_epoch': learned_kernels
+                'val_mask_per_epoch': val_wandb_img_and_masks,
+                'val_internals_per_epoch':val_wandb_internal_representations,
+                'filters_per_epoch': learned_kernels,
+                'train_mask_per_epoch': train_wandb_img_and_masks,
+                'train_internals_per_epoch':train_wandb_internal_representations,
             }
         else:
             epoch_metrics = {
-                'train_epochs': epoch_train_metrics,
+                'train_epochs': epoch_train_metrics[0],
                 'val_epochs': epoch_val_metrics[0],
                 'epoch': epoch,
             }

workstation/src/utils.py
@@ -9,83 +9,54 @@ import json
 torch.manual_seed(50)
 
 """loading config file"""
-def load_yaml(config_path="./src/config/config.yaml"):
+def load_yaml(config_path="./ACTIA/workstation/src/config/config.yaml"):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
-"""mask processing"""
-class cityscapesMaskProcessor:
+class UnetMaskProcessor:
     def __init__(self):
-        """cityscapes color_map"""
-        self.color_map = np.array([
-            [128, 64, 128],  # Road
-            [244, 35, 232],  # Sidewalk
-            [70, 70, 70],  # Building
-            [102, 102, 156],  # Wall
-            [190, 153, 153],  # Fence
-            [153, 153, 153],  # Pole
-            [250, 170, 30],  # Traffic Light
-            [220, 220, 0],  # Traffic Sign
-            [107, 142, 35],  # Vegetation
-            [152, 251, 152],  # Terrain
-            [70, 130, 180],  # Sky
-            [220, 20, 60],  # Person
-            [255, 0, 0],  # Rider
-            [0, 0, 142],  # Car
-            [0, 0, 70],  # Truck
-            [0, 60, 100],  # Bus
-            [0, 80, 100],  # Train
-            [0, 0, 230],  # Motorcycle
-            [119, 11, 32],  # Bicycle
-        ])
-        self.class_labels = {0: 'road', 1: 'sidewalk', 2: 'building', 3: 'wall',
-                             4: 'fence', 5: 'pole', 6: 'traffic light', 7: 'traffic sign',
-                             8: 'vegetation', 9: 'terrain', 10: 'sky', 11: 'person', 12: 'rider',
-                             13: 'car', 14: 'truck', 15: 'bus', 16: 'train', 17: 'motorcycle',
-                             18: 'bicycle'}
-
-    def one_hotting_mask(self, mask):
-        mask = mask[:, :, :3]  # remove alpha channel
-        height, width = mask.shape[:2]
-        num_classes = len(self.color_map)
-        one_hot_mask = np.zeros((num_classes, height, width), dtype=np.uint8)
+        """Cityscapes color_map"""
+        self.color_map = [
+            (128, 64, 128),   # Road
+            (244, 35, 232),   # Sidewalk
+            (70, 70, 70),     # Building
+            (102, 102, 156),  # Wall
+            (190, 153, 153),  # Fence
+            (153, 153, 153),  # Pole
+            (250, 170, 30),   # Traffic Light
+            (220, 220, 0),    # Traffic Sign
+            (107, 142, 35),   # Vegetation
+            (152, 251, 152),  # Terrain
+            (70, 130, 180),   # Sky
+            (220, 20, 60),    # Person
+            (255, 0, 0),      # Rider
+            (0, 0, 142),      # Car
+            (0, 0, 70),       # Truck
+            (0, 60, 100),     # Bus
+            (0, 80, 100),     # Train
+            (0, 0, 230),      # Motorcycle
+            (119, 11, 32),    # Bicycle
+        ]
+        self.class_labels={0: 'Road', 1: 'Sidewalk', 2: 'Building', 3: 'Wall', 4: 'Fence', 
+                           5: 'Pole', 6: 'Traffic Light', 7: 'Traffic Sign', 8: 'Vegetation', 
+                           9: 'Terrain', 10: 'Sky', 11: 'Person', 12: 'Rider', 13: 'Car', 
+                           14: 'Truck', 15: 'Bus', 16: 'Train', 17: 'Motorcycle', 18: 'Bicycle'}
+        self.num_classes = len(self.color_map)
         
-        # Efficiently calculate the one-hot mask
-        for class_index, color in enumerate(self.color_map):
-            one_hot_mask[class_index] = np.all(mask == color, axis=-1).astype(np.uint8)
+    def one_hotting_mask(self, mask):
+        """Convert RGB mask to one-hot encoded mask"""
+        height, width,_ = mask.shape
+        one_hot_mask = np.zeros((self.num_classes, height, width), dtype=np.uint8)
 
+        # Iterate over each pixel to find class index
+        for class_index, color in enumerate(self.color_map):
+            binary_mask = np.all(mask == color, axis=-1)
+            one_hot_mask[class_index] = binary_mask
         return one_hot_mask
 
-
-class bdd10kMaskProcessor(cityscapesMaskProcessor):
-    def __init__(self):
-        super().__init__()
-
-    def one_hotting_mask(self, mask):
-        if mask.shape==2:
-            height, width = mask.shape
-            num_classes = len(self.color_map)
-            one_hot_mask = np.zeros((num_classes, height, width), dtype=np.uint8)
-
-            # Efficiently calculate the one-hot mask
-            for class_index in range(num_classes):
-                one_hot_mask[class_index] = (mask == class_index).astype(np.uint8)
-                return one_hot_mask
-        else:
-            mask = mask[:, :, :3]  # remove alpha channel
-            height, width = mask.shape[:2]
-            num_classes = len(self.color_map)
-            one_hot_mask = np.zeros((num_classes, height, width), dtype=np.uint8)
-            
-            # Efficiently calculate the one-hot mask
-            for class_index, color in enumerate(self.color_map):
-                one_hot_mask[class_index] = np.all(mask == color, axis=-1).astype(np.uint8)
-
-            return one_hot_mask
-    
 class datasetSplitter:
-    def __init__(self, dataset, batch_size, test_split=0.3, random_seed=50):
+    def __init__(self, dataset, batch_size, test_split=0.2, random_seed=500):
         self.dataset = dataset
         self.batch_size = batch_size
         self.test_split = test_split
@@ -106,8 +77,8 @@ class datasetSplitter:
         test_sub_data = Subset(self.dataset,test_indices)
 
         # Create data loaders
-        self.train_loader = DataLoader(train_sub_data, batch_size=self.batch_size ,num_workers=4,shuffle=True)
-        self.test_loader = DataLoader(test_sub_data, batch_size=self.batch_size, num_workers=4,shuffle=False)
+        self.train_loader = DataLoader(train_sub_data, batch_size=self.batch_size ,num_workers=0,shuffle=True,pin_memory=False)
+        self.test_loader = DataLoader(test_sub_data, batch_size=self.batch_size, num_workers=0,shuffle=False,pin_memory=False)
 
         return self.train_loader, self.test_loader
     